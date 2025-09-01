829cc6fec51f6cdfbff0536a0d759fc386a8d0ec
Name <example@email.com>
2025-09-01 18:12:24 +0700

    Tugas01

---
+++ Tugas01.c
@@ -0,0 +1,12 @@
+//Tugas Git Started : Version Control For New Coders - 01 Sept 2025
+
+#include <stdio.h>
+
+int main();
+{
+    char *name = "Willson Matthew Noverendy";
+    char *lang = "Python";
+    printf("Hello, my name is %s and my favorite programming language is %s.", name, lang);
+
+    return 0;
+}
