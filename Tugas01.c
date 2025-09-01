06c9029ee48d429dc22174080733d3e933bbb415
Name <example@email.com>
2025-09-01 18:15:18 +0700

    Seharusnya tidak menggunakan (;)

--- Tugas01.c
+++ Tugas01.c
@@ -2,10 +2,9 @@
 
 #include <stdio.h>
 
-int main();
-{
-    char *name = "Willson Matthew Noverendy";
-    char *lang = "Python";
+int main(){
+    char* name = "Willson Matthew Noverendy";
+    char* lang = "Python";
     printf("Hello, my name is %s and my favorite programming language is %s.", name, lang);
 
     return 0;
